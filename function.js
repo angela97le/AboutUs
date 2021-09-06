b454f862590747b4fac28e0a3e15305b819e9916
Angela L <angela97le@g.ucla.edu>
2021-09-06 14:16:57 -0700

    HTML, CSS, JS

---
+++ function.js
@@ -0,0 +1,92 @@
+<!--Allows JQuery to be used across page--!>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!--Click on about button for description to slide down and up. Each person has their own function.--!>
+<script type="text/javascript"> 
+
+$(".myGary").click(function(){
+    $("#garyDesc").slideToggle();
+  });
+
+$(".myKaren").click(function(){
+    $("#karenDesc").slideToggle();
+  });
+
+$(".myGreg").click(function(){
+    $("#gregDesc").slideToggle();
+  });
+
+$(".myUlrich").click(function(){
+    $("#ulrichDesc").slideToggle();
+  });
+
+$(".about-btn").click(function(){
+    $(this).children("i").toggleClass("fa-chevron-down fa-chevron-up");
+  });
+
+</script>
+
+
+
+
+<script type="text/javascript"> 
+
+$(".myBrian").click(function(){
+    $("#brianDesc").slideToggle();
+  });
+
+$(".myDeb").click(function(){
+    $("#debDesc").slideToggle();
+  });
+
+$(".myDave").click(function(){
+    $("#daveDesc").slideToggle();
+  });
+
+</script>
+
+
+
+
+<script type="text/javascript"> 
+
+$(".myKyungjin").click(function(){
+    $("#kyungjinDesc").slideToggle();
+  });
+
+$(".myGabriella").click(function(){
+    $("#gabriellaDesc").slideToggle();
+  });
+
+$(".myMaggie").click(function(){
+    $("#maggieDesc").slideToggle();
+  });
+
+</script>
+
+
+
+
+<script type="text/javascript"> 
+
+$(".myJon").click(function(){
+    $("#jonDesc").slideToggle();
+  });
+
+$(".myBhumika").click(function(){
+    $("#bhumikaDesc").slideToggle();
+  });
+
+$(".myShannon").click(function(){
+    $("#shannonDesc").slideToggle();
+  });
+
+$(".myTifany").click(function(){
+    $("#tifanyDesc").slideToggle();
+  });
+
+$(".myJason").click(function(){
+    $("#jasonDesc").slideToggle();
+  });
+
+</script>
